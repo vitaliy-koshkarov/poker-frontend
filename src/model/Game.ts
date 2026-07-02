891c9510e47944b0635af2f7b2f6d46eb521c50b
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2026-07-02 19:08:11 +0100

    Add fields for blinds and min raise in Game class

--- src/model/Game.ts
+++ src/model/Game.ts
@@ -1,24 +1,31 @@
 export class Game {
     id: number;
+    name: string;
+    creatorPlayerId: number;
     currentPlayers: number;
     maxPlayers: number;
     buyIn: number;
     status: number;
-    name: string;
-    creatorPlayerId: number;
     dealerId: number;
     activePlayerId: number;
+    smallBlind: number;
+    bigBlind: number;
+    minRaise: number;
 
-    constructor(id: number, currentPlayers: number, maxPlayers: number, buyIn: number, status: number, name: string,
-                creatorPlayerId: number, dealerId: number, activePlayerId: number) {
+    constructor(id: number, name: string, creatorPlayerId: number, currentPlayers: number, maxPlayers: number,
+                buyIn: number, status: number, dealerId: number, activePlayerId: number,
+                smallBlind: number, bigBlind: number, minRaise: number) {
         this.id = id;
+        this.name = name;
+        this.creatorPlayerId = creatorPlayerId;
         this.currentPlayers = currentPlayers;
         this.maxPlayers = maxPlayers;
         this.buyIn = buyIn;
         this.status = status;
-        this.name = name;
-        this.creatorPlayerId = creatorPlayerId;
         this.dealerId = dealerId;
         this.activePlayerId = activePlayerId;
+        this.smallBlind = smallBlind;
+        this.bigBlind = bigBlind;
+        this.minRaise = minRaise;
     }
 }
